a23419c29299f7d15e871a7936303255efb9939e
gowrishvarma <106875430+gowrishvarma2003@users.noreply.github.com>
2025-03-21 02:25:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/api/programs', programRoutes);
 app.use('/api/courses', courseRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.DB_URl)
+mongoose.connect("mongodb+srv://9hacks:nkpacmfb8m@cluster0.tasptqf.mongodb.net/")
     .then(() => {
         console.log('MongoDB connected');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
